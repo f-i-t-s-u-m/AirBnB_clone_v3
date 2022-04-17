cc8b4564379bdea85f2032cecf2e5a8b66ba339a
f-i-t-s-u-m <fibabirr@gmail.com>
2022-04-17 21:27:11 +0300

    add task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,20 @@ index file
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """ show stats of records
+    """
+    classes = {"Amenity", "BaseModel", "City",
+           "Place", "Review", "State", "User"}
+
+    data = {}
+    for val in classes:
+        data[val] = storage.count(val)
+
+    return jsonify(data)
 
 
 @app_views.route('/status', strict_slashes=False)
