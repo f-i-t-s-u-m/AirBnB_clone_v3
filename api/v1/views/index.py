c4adcc758917abcbc95e3d48f5ed415727463747
f-i-t-s-u-m <fibabirr@gmail.com>
2022-04-17 20:45:43 +0300

    task 3

---
+++ api/v1/views/index.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+index file
+"""
+
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """ server status """
+    return jsonify({'status':'OK'})
