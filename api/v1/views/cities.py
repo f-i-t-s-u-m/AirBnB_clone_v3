cfcf8996e1551d2d85ecb073519fb8bab83fda10
f-i-t-s-u-m <fibabirr@gmail.com>
2022-04-18 23:34:47 +0300

    fix all

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -2,11 +2,12 @@
 """ state file
 """
 
-from flask import jsonify, abort, request, make_response
+from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
 from models.state import State
 from models.city import City
+
 
 @app_views.route('/states/<state_id>/cities')
 def cities(state_id):
