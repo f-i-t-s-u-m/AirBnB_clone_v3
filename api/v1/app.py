27859e56c89370a60696f3e9541196e82107b68f
f-i-t-s-u-m <fibabirr@gmail.com>
2022-04-18 18:20:34 +0300

    fix app

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,11 +6,13 @@ create python file
 from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
-import os
+from os import getenv
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
 
 
 @app.errorhandler(404)
@@ -26,6 +28,7 @@ def teardown(self):
 
 
 if __name__ == '__main__':
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', '5000')
-    app.run(host=host, port=port, threaded=True)
+    debug = getenv('HBNB_MODE', True)
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, threaded=True, debug=debug)
